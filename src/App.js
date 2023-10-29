ddf5bb1e289f6107a8b23208686365186df47d88
jabari <jabarimitchellnya@gmail.com>
2023-10-29 13:54:39 -0400

    creating section component that has css function for background

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Home from "./components/Home";
+import Section from "./components/Section";
+
+function App() {
+  return (
+    <div className="App">
+     {/* <Header /> */}
+      <Home />
+    </div>
+  )
+}
+
+export default App;
